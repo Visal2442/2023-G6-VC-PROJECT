2f4734b8d87dbef4a019ca576743c94e7edf0db7
sreyrea <hansreyrea2222@gmail.com>
2023-07-04 20:51:16 +0700

    [VG-110]:create view footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+    <footer>
+      <the-footer></the-footer>
+    </footer>
+  </div>
 
+</template>
+<script>
+import TheFooter from './components/TheFooter.vue';
+export default {
+  components:{TheFooter},
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
