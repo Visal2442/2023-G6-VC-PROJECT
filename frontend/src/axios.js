a9238b54401a31c12d9a3c11177e8f3a2c1c042f
Visal Sork <visalsork@Visals-Macbook.local>
2023-07-05 14:04:51 +0700

    Debug

--- frontend/src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const user = JSON.parse(window.localStorage.getItem('user'))
-
-axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
-if(user){
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.token
-}
