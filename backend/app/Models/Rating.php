8cfdca7731bfa665d22048c06324fd3f54ff1bc5
sreyrea <hansreyrea2222@gmail.com>
2023-07-05 14:10:26 +0700

    [VG-116]:Update data

--- backend/app/Models/Rating.php
+++ backend/app/Models/Rating.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
-    
-    protected $fillable = ['user_id','propery_id'];
+
+    protected $fillable = ['user_id','property_id'];
 
     public function user()
     {
