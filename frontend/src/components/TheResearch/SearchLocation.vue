b84b1e61dabcdef12362467760cbdd4712c9a181
PHALLY.HOEURNG <PHALLY.HOEURNG@PNC.LAN>
2023-07-11 09:26:20 +0700

    [VG-26]:Display the search results with data

--- frontend/src/components/TheResearch/SearchLocation.vue
+++ frontend/src/components/TheResearch/SearchLocation.vue
@@ -1,50 +1,81 @@
 <template>
-     <div class="search">
-          <div class="research">
-               <input type="text">
-               <v-icon size="large" color="white" icon="mdi mdi-magnify"></v-icon>
-          </div>
-     </div>
+  <div class="search">
+    <div class="searchLocation">
+      <input
+        class="research"
+        type="text"
+        placeholder="search here"
+        v-model="searchQuery"
+      />
+      <v-icon size="large" color="white" icon="mdi mdi-magnify"></v-icon>
+      <div v-if="searchResults.length" class="districtCard">
+        <span
+          class="district"
+          v-for="result in searchResults"
+          :key="result.id"
+          >{{ result.name }}</span
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref, watch } from "vue";
+import axios from "axios";
 
-}
+const URL = "http://127.0.0.1:8000/api/locations/";
+const searchQuery = ref("");
+const searchResults = ref([]);
+watch(searchQuery, async (value) => {
+  try {
+    const response = await axios.get(`${URL} + ?name=${value}`);
+    searchResults.value = response.data.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
 <style scoped>
 .search {
-     /* background: #d62a2a; */
-     display: flex;
-     justify-content: center;
-     align-content: center;
-
-     margin-top: -40%;
-     margin-bottom: 40%;
-
+  text-align: center;
+  margin-top: -40%;
+  margin-bottom: 40%;
 }
-
 .research {
-     width: 50%;
-     /* background: #85c60d; */
+  width: 40.5%;
+  padding: 9px;
+  margin-left: -15px;
+  background: rgba(255, 255, 255, 0.772);
 }
-
 .research input {
-     background: #e2dedec6;
-     width: 80%;
-     padding: 10px;
-     border-radius: 5px 0 0 5px;
-
-
+  outline: none;
+  background: white;
+  width: 80%;
+  padding: 10px;
+  border-radius: 2px 0 0 2px;
 }
-
+.districtCard {
+  width: 43.9%;
+  margin-left: 27.4%;
+  margin-top: -5px;
+  text-align: center;
+  background: white;
+  box-shadow: 0px 0px 6px 0px rgba(128, 128, 128, 0.874);
+}
 .v-icon {
-     background: orange;
-     padding: 22px;
-     border-radius: 0 5px 5px 0;
-     margin-bottom: 3px;
-
-
+  background: orange;
+  padding: 21.5px;
+  border-radius: 0 5px 0px 0;
+  margin-bottom: 3px;
+}
+.district {
+  display: flex;
+  justify-content: center;
+  height: auto;
+}
+.district:hover {
+  background-color: rgb(228, 228, 228);
 }
 </style>
--- frontend/src/store/PropertyStore.js
+++ frontend/src/store/PropertyStore.js
@@ -1,35 +1,52 @@
 import { defineStore } from "pinia";
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref } from "vue";
+import axios from "axios";
 
-export const usePropertyStore = defineStore('property', () => {
-    const properties = ref([]);
-    let currentLat = ref(0);
-    let currentLng = ref(0);
+export const usePropertyStore = defineStore("property", () => {
+  const properties = ref([]);
+//   const districts = ref([]);
+//   const name = ref("");
+  let currentLat = ref(0);
+  let currentLng = ref(0);
 
-    // Get all properties
-    axios.get('/properties').then(res=>{
-        properties.value = res.data.data;
-        console.log(properties.value);
-    }).catch(err=>{
-        console.log(err);
+  // Get all properties
+  axios
+    .get("/properties")
+    .then((res) => {
+      properties.value = res.data.data;
+      //   console.log(properties.value);
     })
-    // Callback function 
-    const showLocation = (location)=>{
-        currentLat.value = location.coords.latitude;
-        currentLng.value =  location.coords.longitude;
-    }
-    const showError = (error)=>{
-        console.log(error);
-    }
-    // Get current location 
-    navigator.geolocation.getCurrentPosition(showLocation, showError);
+    .catch((err) => {
+      console.log(err);
+    });
+  // Get all districts
+//   axios
+//     .get(`/locations/${name.value}`)
+//     .then((res) => {
+//       districts.value = res.data.data;
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
 
-    return {
-        properties,
-        showLocation,
-        currentLat,
-        currentLng,
-        showError
-    }
+  // Callback function
+  const showLocation = (location) => {
+    currentLat.value = location.coords.latitude;
+    currentLng.value = location.coords.longitude;
+  };
+  const showError = (error) => {
+    console.log(error);
+  };
+  // Get current location
+  navigator.geolocation.getCurrentPosition(showLocation, showError);
+
+  return {
+    properties,
+    showLocation,
+    currentLat,
+    currentLng,
+    showError,
+    // districts,
+    // name,
+  };
 });
