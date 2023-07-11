e629dd42808492f669a160304fe6f3cf0f229ff9
Visal Sork <visalsork@Visals-Macbook.local>
2023-07-11 08:14:23 +0700

    [VG-125]: Debug

--- backend/app/Http/Controllers/AuthenticationController.php
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -15,15 +15,39 @@ class AuthenticationController extends Controller
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => ['unique:users','email'],
-            // 'password_confirmation' => ['required'],
-            'password' => ['confirmed'],
-        ]);
+        if ($request->isGoogle) {
+            $user = User::where('email', $request->email)->get();
+            // Attempt to authenticate the user
+            if ($user) {
+                // Authentication successful
+                // $user = Auth::user();
+                $token = $user->createToken('API Token', ['select'])->plainTextToken;
+                return response()->json(["message" => "login success", "user" => $user, 'token'=> $token], 200);
+            } else {
+                // Authentication failed
+                $user = User::create([
+                    'username' => $request->username,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
         
-        if ($validator->fails()) {
-            return response()->json(['success'=>'false', 'errors' => $validator->errors()], 401);
+                $token = $user->createToken('API Token', ['select'])->plainTextToken;
+        
+                return response()->json([
+                    'success' => 'true',
+                    'user' => $user,
+                    'token' => $token,
+                ], 200);
+            }
         }
+            $validator = Validator::make($request->all(), [
+                'email' => ['unique:users', 'email'],
+                // 'password_confirmation' => ['required'],
+                'password' => ['confirmed'],
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['success' => 'false', 'errors' => $validator->errors()], 401);
+            }
         $user = User::create([
             'username' => $request->username,
             'phone_number' => $request->phone_number,
@@ -39,7 +63,7 @@ class AuthenticationController extends Controller
             'token' => $token,
         ], 200);
     }
-    
+
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
@@ -51,18 +75,24 @@ class AuthenticationController extends Controller
 
     public function login(Request $request)
     {
-        // get email and password
-        $credentials = $request->only('email', 'password');
+        $isForm = true;
+        if($request->isGoogle){
+            $user = User::where('email', $request->email)->get();
+            $isForm = false;
+        }
+        if($isForm){
+            // get email and password
+            $credentials = $request->only('email', 'password');
+        }
         // Attempt to authenticate the user
         if (Auth::attempt($credentials)) {
             // Authentication successful
             $user = Auth::user();
             $token = $user->createToken('API Token', ['select'])->plainTextToken;
-            return response()->json(["message" => "login success", "user"=>$user,"token" => $token], 200);
+            return response()->json(["message" => "login success", "user" => $user, "token" => $token], 200);
         } else {
             // Authentication failed
-            return response()->json(['success'=>'false','errors' => 'Invalid email or password'], 401);
+            return response()->json(['success' => 'false', 'errors' => 'Invalid email or password'], 401);
         }
     }
-
 }
--- frontend/src/components/form/LoginForm.vue
+++ frontend/src/components/form/LoginForm.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- Resource: vuetify  -->
   <v-row justify="center" align="center">
-    <img src="../../assets/register.jpg" alt="" width="500" height="500" />
+    <img :src="require('../../assets/register.jpg')" alt="" width="500" height="500" />
     <v-sheet class="m-10 w-33 bg-white pa-8" elevation="4">
-      <img src="../../assets/profileIcon.png" class="mt-5" width="50" />
+      <img :src="require('../../assets/profileIcon.png')" class="mt-5" width="50" />
       <h3 class="text-green-accent-4 mb-5">Login Account</h3>
       <v-expand-transition>
         <v-alert type="error" class="text-red-accent-4 text-left mb-5" v-if="isSuccess && errors" :text='errors'></v-alert>
@@ -13,7 +13,7 @@
 
         <v-text-field type="password" clearable color="green-accent-4" label="Password" name="password" placeholder="Enter password" v-model="password" :rules="rules.passwordRules"></v-text-field>
 
-        <router-link to=""><img src="../../assets/google.png" alt="" width="50" /></router-link>
+        <GoogleLogin :callback="callback" class=" my-5"></GoogleLogin>
 
         <v-btn type="button" v-model="isLoggedIn" @click="login" block class="mt-2 mb-5 bg-green-accent-4 text-white">Login</v-btn>
         <p>Don't have an account? | <router-link :to="{ name: 'Register' }" class="">Register Here</router-link></p>
@@ -23,12 +23,27 @@
 </template>
 <script setup>
 import { ref } from 'vue';
+import { decodeCredential } from 'vue3-google-login';
 import { storeToRefs } from 'pinia';
 import { useAuthStore } from '../../store/AuthStore';
 const authStore = useAuthStore();
 
-const { email, password, isValide, isSuccess, errors, isLoggedIn } = storeToRefs(authStore);
+const { email, password, isValide, isSuccess, errors } = storeToRefs(authStore);
 const { login } = authStore;
+
+const callback =  (res) => {
+    console.log(decodeCredential(res.credential));
+    const userDetail = decodeCredential(res.credential);
+    const user = {
+      username: userDetail.given_name + ' ' +  userDetail.family_name,
+      email:userDetail.email,
+      password:Math.floor(Math.random() * 10000000000),
+      isGoogle:true,
+    }
+    login(user);
+}
+
+
 // Validation rules 
 const rules = ref({
   emailRules: [value => !!value || 'Email is required',],
