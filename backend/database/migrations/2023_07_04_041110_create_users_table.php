f04e939cce91598783dac242b6bfe11701aa68ad
DARATH.PUM <DARATH.PUM@PNC.LAN>
2023-07-13 10:40:30 +0700

    debug recover password

--- backend/database/migrations/2023_07_04_041110_create_users_table.php
+++ backend/database/migrations/2023_07_04_041110_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
                 $table->string('email')->unique();
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password')->nullable();
+                $table->string('verification_code')->nullable();
                 $table->string('role')->default('customer');
                 $table->rememberToken();
                 $table->timestamps();
