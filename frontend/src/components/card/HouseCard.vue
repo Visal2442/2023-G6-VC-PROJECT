f419d043e1baca02fc9f7b0b104d196695bf20bd
Visal Sork <visalsork@Visals-Macbook.local>
2023-07-18 16:35:15 +0700

    [VG-207]: Create dialog view by following mockup.

--- frontend/src/components/card/HouseCard.vue
+++ frontend/src/components/card/HouseCard.vue
@@ -21,7 +21,21 @@
         </div>
         <v-card-actions class="pa-0">
             <v-card-text class="text-green-accent-4 text-md-body-1">${{ property?.price }}/month</v-card-text>
-            <v-rating v-model="rating" density="compact" bg-color="orange-lighten-1"></v-rating>
+            <v-card-text class="text-end cursor-pointer" @click="dialog = !dialog"><v-icon class="mdi mdi-star mr-1" color="amber-lighten-2"></v-icon>4.5</v-card-text>
+
+            <v-dialog v-model="dialog" transition="dialog-top-transition" persistent width="auto">
+                <v-card title="Rate this house !" max-width="400">
+                    <v-card-text class=" text-center">
+                        <v-rating v-model="rating" density="compact" color="amber-lighten-2"  size="x-large"></v-rating>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="deep-orange-accent-3" @click="dialog = !dialog">Cancel</v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green-accent-3" @click="rateStar(rating, property.id)">Rate Now</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- <v-rating v-model="rating" @click="addStar(rating)" density="compact" color="amber-lighten-2" size="small" half-increments></v-rating> -->
         </v-card-actions>
         <v-card-actions>
             <div class="d-flex align-center">
@@ -35,14 +49,12 @@
 </template>
 
 <script setup>
-import { defineProps,defineEmits, ref } from "vue";
+import { defineProps, defineEmits,ref } from "vue";
 import BaseButton from "../widget/BaseButton.vue";
 import { useRouter } from "vue-router";
-
-
-const emits = defineEmits(['addToWishlist']);
 const router = useRouter();
 
+const emits = defineEmits(['addToWishlist', 'rateStar']);
 defineProps(["property"]);
 
 const addToWishlist = (property_id) => {
@@ -54,8 +66,23 @@ const getDetail = (property_id) => {
 };
 
 
-// Rating 
-const rating = ref(3);
+// Rating
+const rating = ref(null);
+const dialog = ref(false)
+
+const rateStar = (rating, property_id)=>{
+    const property =ref({
+        rating:rating,
+        property_id:property_id
+    })
+    if(rating < 1){
+        alert('Please rate the house !');
+    }
+    else{
+        emits('rateStar', property.value)
+        dialog.value = false;
+    }
+}
 
 </script>
 
