25ec12ba713d2a96ae1f08c0afa76d06eb7d76bc
Visal Sork <visalsork@Visals-Macbook.local>
2023-07-19 19:03:36 +0700

    [VG-164]: Create a view for a form.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
-    <the-navbar></the-navbar>
+    <!-- FOR CUSTOMER PAGE  -->
+    <the-navbar v-if="!route.path.includes('dashboard')"></the-navbar>
     <!-- RESOURCE : https://www.youtube.com/watch?v=X4I6zUEM40A -->
     <router-view v-slot="{ Component }">
-      <transition name="route" mode="out-in">
-        <component :is="Component"></component>
-      </transition>
+      <div v-if="!route.path.includes('dashboard')">
+        <transition name="route" mode="out-in">
+          <component :is="Component"></component>
+        </transition>
+      </div>
     </router-view>
-    <the-footer></the-footer>
+    <the-footer v-if="!route.path.includes('dashboard')"></the-footer>
+
+    <!-- FOR ADMIN PAGE  -->
+    <v-app>
+      <dashboard-navbar v-if="route.path.includes('dashboard')"></dashboard-navbar>
+      <v-main>
+        <!-- <dashboard-header></dashboard-header> -->
+        <router-view />
+      </v-main>
+    </v-app>
   </div>
 </template>
 
 <script setup>
+// import DashboardHeader from './components/partials/DashboardHeader.vue'
+import DashboardNavbar from './components/partials/DashboardSidebar.vue';
 import TheFooter from './components/partials/TheFooter.vue';
-
+import { useRoute } from 'vue-router';
+const route = useRoute()
 </script>
 
 <style scoped>
@@ -22,19 +37,23 @@ import TheFooter from './components/partials/TheFooter.vue';
   background-image: linear-gradient(147deg, #f9fcff 0%, #f6f6f6 74%);
   font-family: 'Signika Negative', sans-serif;
 }
+
 /* route transition  */
-.route-enter-from{
-  opacity:0;
+.route-enter-from {
+  opacity: 0;
   transform: translateX(100px);
 }
-.route-enter-active{
+
+.route-enter-active {
   transition: all 0.3s ease-out;
 }
-.route-leave-to{
+
+.route-leave-to {
   opacity: 0;
   transform: translateX(-100px);
 }
-.route-leave-active{
+
+.route-leave-active {
   transition: all 0.3s ease-in;
 }
 </style>
---
+++ frontend/src/components/partials/DashboardHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-toolbar id="navbar" class="bg-white py-5" elevation="3">
+        <v-toolbar-title>
+            <div class="d-flex align-center">
+                <h4 class="">DASHBOARD</h4>
+            </div>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs d-flex align-center pr-10">
+            <v-toolbar-title class="mr-5">visal@gmail.com</v-toolbar-title>
+            <v-avatar id="logged-in">
+                <v-img :src="require('../../assets/profile2.jpeg')"></v-img>
+            </v-avatar>
+            <v-menu id="menu" activator="#logged-in">
+                <v-list>
+                    <v-list-item>
+                        <v-btn variant="plain" :to="{ name: 'Post' }" prepend-icon="mdi mdi-login">Post</v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn variant="plain" :to="{ name: 'Login' }" prepend-icon="mdi mdi-account">Login</v-btn>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+#navbar {
+  position: sticky;
+  top: 0;
+  z-index: 2000;
+}
+</style>
