2cf984064fbfc601f2aaafe0baabd0d34cc1a03f
sreyrea <hansreyrea2222@gmail.com>
2023-07-21 10:15:04 +0700

    [VG-200]:Front-end controller to send email request to be landlord

--- frontend/src/components/form/ContactForm.vue
+++ frontend/src/components/form/ContactForm.vue
@@ -1,5 +1,8 @@
 <template>
-     <v-container fluid class="d-flex ">
+    
+  
+
+    <v-container fluid class="d-flex ">
         <v-seet class="d-flex flex-column flex-1 w-100 justify-center bg-green aling-center" elevation="4">
             <h2 class="text-center mb-8">OUR INFORMATION</h2>
             <div class="our-phone d-flex flex-column mb-9 ml-16" >
@@ -27,23 +30,30 @@
             
 
         </v-seet>
-        <v-sheet class="m-10 w-100 bg-white pa-8" elevation="4">
+        <v-sheet class="m-10 w-100 bg-white pa-8 " elevation="4">
+            <v-alert v-if="alert"
+            class="mb-10"
+            type="success"
+            title="Alert title"
+            text="Your request was successfully!"
+            @click="clearAlert()"
+        ></v-alert>
             <h2 class="text-center mb-9">CONTAC US</h2>
             <v-form fast-fail x="d-flex " v-model="isValide">
                 <div class=" d-flex">
                     <v-text-field type="text" clearable color="green-accent-4" label="Username" name="username" placeholder="username"
-                        variant="outlined" class="mr-3" ></v-text-field>
+                        variant="outlined" class="mr-3" :rules="rules.usernameRules" v-model="username" ></v-text-field>
                     <v-text-field type="text" clearable color="green-accent-4" label="Phone" name="phoneNumber" placeholder="Phone Number"
-                        variant="outlined" ></v-text-field>
+                        variant="outlined" :rules="rules.phoneRules" v-model="phone_number"  ></v-text-field>
                 </div>
                 <v-text-field type="Email" clearable color="green-accent-4" label="Email" name="email" placeholder="Your Email Address"
-                        variant="outlined" ></v-text-field>
+                :rules="rules.emailRules" v-model="email"  variant="outlined" ></v-text-field>
                 <div class="d-flex">
                     <v-textarea clearable label="Your Message" placeholder="Message" color="green-accent-4"
-                        variant="outlined"></v-textarea>
+                    :rules="rules.messageRules" v-model="message" variant="outlined"></v-textarea>
                 </div>
                 <div class="d-flex justify-end">
-                    <v-btn type="button" class=" mb-5 bg-green-accent-4 text-white px-15" >Submit</v-btn>
+                    <v-btn type="button" class=" mb-5 bg-green-accent-4 text-white px-15"  :disabled="!isValide" @click="userContact()">Submit</v-btn>
                 </div>
             </v-form>
         </v-sheet>
@@ -51,10 +61,57 @@
   
 </template>
 
-<script>
-export default {
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+let isValide = ref(false);
+const username = ref(null);
+const message = ref(null);
+const email = ref(null);
+const phone_number = ref(null);
+const alert = ref(false);
 
+const remove = ()=>{
+        username.value ="",
+        phone_number.value ="",
+        email.value ="",
+        message.value =""
 }
+const clearAlert = ()=>{
+       alert.value = false
+}
+
+const userContact = () => {
+    const user = {
+        username: username.value,
+        phone_number: phone_number.value,
+        email: email.value,
+        message_request: message.value,
+    }
+    axios.post('/requestLandlord', user)
+        .then(response => {
+            console.log(response.data);
+            alert.value  = true;
+            remove()
+        })
+        .catch(error => {
+            console.log(error);
+        }
+        );
+    console.log(user);
+}
+// Validation rules 
+const rules = ref({
+    usernameRules: [value => !!value || 'Username is required'],
+    emailRules: [value => !!value ||  'Email is required'],
+    phoneRules: [value => !!value || 'Phone Number is required ',
+    value => /^\+?[0]\d{8,20}$/.test(value) || 'Phone Number is invalid'],
+    messageRules: [value => !!value || 'message is required']
+
+});
+
+
+
 </script>
 
 <style>
